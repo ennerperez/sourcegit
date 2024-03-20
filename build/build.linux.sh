49c5756997ba2f14c13f5a49000538408d2ebac4
Enner Pérez <ennerperez@gmail.com>
2024-03-20 05:32:08 -0500

    Updater Build Command

--- build/build.linux.sh
+++ build/build.linux.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rm -rf SourceGit
+dotnet publish ../src/Updater/Updater.csproj -c Release -r linux-x64 -o SourceGit -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
 dotnet publish ../src/SourceGit/SourceGit.csproj -c Release -r linux-x64 -o SourceGit -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
 cp resources/SourceGit.desktop.template SourceGit/SourceGit.desktop.template
 cp resources/App.icns SourceGit/SourceGit.icns
